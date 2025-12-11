76cd53c3dd76a6a28d691ccc8bf3bb221762c8c6
unknown <qkdrk7777775@gmail.com>
2025-12-11 17:13:54 +0900

    fix: 자기 댓글은 자기 혹은 관리자만 수정 삭제 가능하게  수정

--- backend/routes/file.py
+++ backend/routes/file.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter,Depends
 
 from controllers import file
+from utils import verify_token
 
 router = APIRouter()
-router.add_api_route("/upload",file.upload, methods=["POST"])
+router.add_api_route("/upload",file.upload, methods=["POST"], dependencies=[Depends(verify_token)])
